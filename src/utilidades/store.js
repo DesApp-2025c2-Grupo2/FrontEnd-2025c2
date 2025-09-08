462b0b11442533961ea10e3e6991e06cc3a4993a
jblenis <jonathan.lenis@estudiantes.unahur.edu.ar>
2025-09-08 16:52:13 -0300

    feat(situacionesTerapeuticas): implementa pagina situaciones terapéuticas
    -Integración de Redux para manejar el estado de las situaciones teraapeuticas
    -Implementación de funciones para editar, activar/desactivar y agregar situaciones terapeuticas
    -Mensajes de retroalimentación usando Snackbar.

--- src/utilidades/store.js
+++ src/utilidades/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import especialidadesReducer from '../store/especialidadesSlice';
+import situacionesReducer from '../store/situacionesTerapeuticasSlice';
 
 export const store = configureStore({
   reducer: {
     especialidades: especialidadesReducer,
+    situaciones: situacionesReducer,
   },
 });
 
