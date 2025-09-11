c825424d07d177216281a85f62c90b5a42d7160f
Oscar Garcia <oscarogarciajr@gmail.com>
2025-09-10 21:58:19 -0300

    feat(grupos-familiares): implementacion de pantalla

---
+++ src/components/DialogAgregarAfiliado.jsx
@@ -0,0 +1,152 @@
+
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { Box, Button, Card, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import EmailIcon from '@mui/icons-material/Email';
+import AddIcon from '@mui/icons-material/Add';
+import PhoneIcon from '@mui/icons-material/Phone';
+import HomeIcon from '@mui/icons-material/Home';
+
+// Función para pruebas
+const getNombrePlan = function(idPlan, planes) {
+  return planes.find(e => idPlan === e.id).nombre
+}
+
+//Lista de parentescos
+const parentescos = [
+    {id: 1, nombre: "Titular"},
+    {id: 2, nombre: "Cónyuge"},
+    {id: 3, nombre: "Hijo"},
+    {id: 4, nombre: "Familiar a cargo"},
+]
+//Funciones para pruebas
+const getNombreParentesco = function(idParentesco, parentescos) {
+  return parentescos.find(e => idParentesco === e.id).nombre
+}
+
+export default function DialogAgregarAfiliado({grupo, open, onClose, planes}){
+    const [parentescoSeleccionado, setParentescoSeleccionado] = useState(null);
+    
+      const handleChange = (event) => {
+        setParentescoSeleccionado(event.target.value);
+      };
+    
+      const handleGuardar = () => {
+        //Por ahora, lo dejaremos así hasta tener la conexión con el Back
+        onClose();
+      };
+    
+    return (
+        <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
+                  <DialogTitle>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                      <PersonAddIcon color="secondary"/>
+                      <Typography variant="h6" color="#555555ff">Agregar integrante al Grupo Familiar</Typography>
+                    </Stack>
+                  </DialogTitle>
+            
+                  <DialogContent dividers>
+                    <Typography variant="subtitle1" color="#555555ff">
+                      <Box component="span" fontWeight="bold"> Grupo Familiar: </Box>Familia {grupo.nombre}
+                    </Typography>
+            
+                    <Typography variant="body2" color="text.secondary">
+                      Plan Médico: {getNombrePlan(grupo.planMedico, planes)} (Se usará por defecto)
+                    </Typography>
+        
+                    <TextField
+                    required
+                    id="nombre-integrante"
+                    label="Nombre"
+                    />
+                    <TextField
+                    required
+                    id="apellido-integrante"
+                    label="Apellido"
+                    />
+                    <TextField
+                    required
+                    id="dni-integrante"
+                    label="Número de documento"
+                    />
+                    <FormControl fullWidth sx={{mt: 2}}>
+                        <InputLabel id="plan-label"></InputLabel>
+                        <Select
+                        labelId="parentesco-label"
+                        value={parentescoSeleccionado}
+                        onChange={handleChange}
+                        label="Seleccione parentesco"
+                        sx={{color: "black"}}
+                        >
+                            {parentescos.map((parentesco) => (
+                                <MenuItem key={parentesco.id} value={parentesco.nombre}>
+                                {parentesco.nombre}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <TextField
+                    label="Fecha de nacimiento"
+                    type="date"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    fullWidth
+                    sx={{mt: 2}}
+                    />
+
+                    <Card>
+                        <CardContent>
+                            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                                <EmailIcon/>
+                                <Typography> Emails</Typography>
+                            </Box>
+                            <TextField placeholder="Agregar email"></TextField>
+                            <Button>
+                                <AddIcon color="secondary"/>
+                            </Button>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                                <PhoneIcon/>
+                                <Typography> Teléfonos</Typography>
+                            </Box>
+                            <TextField placeholder="Agregar teléfono"></TextField>
+                            <Button>
+                                <AddIcon color="secondary"/>
+                            </Button>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent>
+                            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                                <HomeIcon/>
+                                <Typography> Direcciones</Typography>
+                            </Box>
+                            <TextField placeholder="Agregar direcciones"></TextField>
+                            <Button>
+                                <AddIcon color="secondary"/>
+                            </Button>
+                        </CardContent>
+                    </Card>
+        
+                  </DialogContent>
+            
+                  <DialogActions>
+                    <Button onClick={onClose} sx={{ textTransform: "none" }}  color="secondary">
+                      Cancelar
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      sx={{ textTransform: "none" }}
+                      onClick={handleGuardar}
+                    >
+                      Agregar integrante
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+    )
+}
---
+++ src/components/TarjetaAfiliadoMuestra.jsx
@@ -0,0 +1,48 @@
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import PersonIcon from '@mui/icons-material/Person';
+
+//Lista de parentescos
+const parentescos = [
+    {id: 1, nombre: "Titular"},
+    {id: 2, nombre: "Cónyuge"},
+    {id: 3, nombre: "Hijo"},
+    {id: 4, nombre: "Familiar a cargo"},
+]
+//Funciones para pruebas
+const getNombreParentesco = function(idParentesco, parentescos) {
+  return parentescos.find(e => idParentesco === e.id).nombre
+}
+
+export default function TarjetaAfiliadoMuestra({afiliado, grupo}){
+    return (
+        <Card>
+            <CardContent sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <PersonIcon sx={{ fontSize: 40 }} color="secondary"/>
+                <Box sx={{display: "flex", flexDirection: "column", gap: 1.5}}>  
+                    <Typography variant="h5" color="black">{afiliado.apellido}, {afiliado.nombre}</Typography>
+                    <Typography variant="p" color="textSecondary">
+                        <Box component="span" fontWeight="bold">DNI:</Box> {afiliado.dni} | {" "}
+                        <Box component="span" fontWeight="bold">Credencial: </Box> {grupo.id}-{afiliado.nroIntegrante} 
+                    </Typography>
+                
+                    <Box sx={{ display: "flex", alignItems: "center", gap: 2, mt: 1}}>
+                        <Box sx={{ 
+                        mt: 1,
+                        alignSelf: "start",
+                        backgroundColor: "transparent",
+                        border: "1px solid",
+                        borderColor: "secondary",
+                        color: "secondary",
+                        px: 2,
+                        py: 0.3,
+                        borderRadius: "999px",
+                        display: "inline-block",
+                        fontWeight: "bold",}}>
+                        {getNombreParentesco(afiliado.parentesco, parentescos)}
+                        </Box>
+                    </Box>
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
